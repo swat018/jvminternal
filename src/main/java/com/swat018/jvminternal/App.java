7afe79d63b4e95158a0c3425b546d48e16f091d0
jinwoo park <swat018@gmail.com>
2020-07-06 23:57:21 +0900

    JVM 구조

--- src/main/java/com/swat018/jvminternal/App.java
+++ src/main/java/com/swat018/jvminternal/App.java
@@ -1,8 +1,21 @@
 package com.swat018.jvminternal;
 
-public class App {
-    
-    public static void main(String[] args) {
+import java.util.List;
 
+public class App {
+
+    static String myName;
+
+    static {
+        myName = "jinwoo";
+    }
+
+    private String foo = "bar";
+
+    public static void main(String[] args) {
+//        System.out.println(App.class.getClassLoader());
+//        System.out.println(List.class.getClassLoader());
+        Thread.currentThread();
+        System.out.println(App.class.getSuperclass());
     }
 }
---
+++ src/main/java/com/swat018/jvminternal/jinwoo.java
@@ -0,0 +1,8 @@
+package com.swat018.jvminternal;
+
+public class jinwoo {
+
+    public void work() {
+        System.out.println(App.myName);
+    }
+}
